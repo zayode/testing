0de7f6a6537b04a922ce42c4ba3a0994b1ddf027
Zayod <rouzen447@gmail.com>
2022-12-25 17:23:47 +0300

    first commit

---
+++ app/src/main/java/com/example/kitchen/student.java
@@ -0,0 +1,49 @@
+package com.example.kitchen;
+
+public class student {
+    int num;
+    String name;
+    String email;
+    String id;
+    String faver;
+    int gpa;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFaver() {
+        return faver;
+    }
+
+    public void setFaver(String faver) {
+        this.faver = faver;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
